52e1a92ab48852e85ae61e2d4a1766ccfed2e033
ariffammobox <ariff.a@ammoboxstudios.com>
2023-12-18 10:33:00 +0800

    refactor: removed comment added to README.md

--- Send-DiscordWebhook.ps1
+++ Send-DiscordWebhook.ps1
@@ -14,21 +14,3 @@ try {
 catch {
   Write-Error "Error sending webhook: $_"
 }
-
-# $WebhookContent = @{
-#   embeds = @(
-#     @{
-#       title       = $Title
-#       description = $Description
-#       color       = $Color
-#       fields      = @($Fields)
-#       author      = @{
-#         name     = $AuthorName
-#         icon_url = $AuthorIconUrl
-#       }
-#       thumbnail   = @{
-#         url = $ThumbnailUrl
-#       }
-#     }
-#   )
-# } | ConvertTo-Json -Depth 4
