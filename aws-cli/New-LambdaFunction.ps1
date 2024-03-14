a1851ee7422428f18fe33319b588218631a5b0e4
ariffammobox <ariff.a@ammoboxstudios.com>
2024-03-14 10:48:30 +0800

    feat: reworked to support environment variables refactor: added checking to verify function state

--- aws-cli/New-LambdaFunction.ps1
+++ aws-cli/New-LambdaFunction.ps1
@@ -1,20 +1,23 @@
 $csvFilePath = "${PSScriptRoot}\functions.csv"
 
-$expectedHeaders = @("Name", "Runtime", "Role", "Handler", "Profile", "Region")
+# Check if the CSV file exists
+if (!(Test-Path -Path $csvFilePath)) {
+  Write-Output "$csvFilePath doesn't exist"
+  return
+}
+
+$expectedHeaders = @("Name", "Runtime", "Role", "Handler", "Profile", "Region", "EnvironmentVariables")
 $actualHeaders = (Get-Content -Path $csvFilePath -TotalCount 1) -split ',' | ForEach-Object { $_.Trim() }
 $missingHeaders = $expectedHeaders | Where-Object { $_ -notin $actualHeaders }
 
+# Check if the expected headers are missing
 if ($missingHeaders.Count) {
   Write-Host "CSV doesn't have expected headers: $($expectedHeaders -join ','). "
   Write-Host "Actual headers: $($actualHeaders -join ',')."
   return
 } 
-$functions = Import-Csv $csvFilePath
 
-if (!(Test-Path -Path $csvFilePath)) {
-  Write-Output "$csvFilePath doesn't exist"
-  return
-}
+$functions = Import-Csv $csvFilePath
 
 foreach ($function in $functions) {
   $functionName = $function.Name
@@ -23,7 +26,9 @@ foreach ($function in $functions) {
   $handler = $function.Handler
   $awsProfile = $function.Profile
   $awsRegion = $function.Region
+  $EnvironmentVariables = $function.EnvironmentVariables
 
+  # Create the Lambda function
   aws lambda create-function `
     --function-name $functionName `
     --role $role `
@@ -33,10 +38,29 @@ foreach ($function in $functions) {
     --region $awsRegion `
     --zip-file fileb://lambda.zip
 
-  Start-Sleep -Seconds 1
+  # Wait for the function to become active
+  do {
+    $functionState = aws lambda get-function --function-name $functionName --profile $awsProfile --region $awsRegion --output json | ConvertFrom-Json | % Configuration | % State
+    Write-Output "$functionName is $functionState"
+    Start-Sleep -Seconds 1
+  } until ($functionState -eq "Active")
 
-  aws lambda publish-version --function-name $functionName --profile $awsProfile --region $awsRegion
+  # Update function configuration
+  aws lambda update-function-configuration --function-name $functionName --timeout 30 --profile $awsProfile --region $awsRegion --output table
 
-  aws lambda create-alias --function-name $functionName --function-version 1 --name production --profile $awsProfile --region $awsRegion
-  aws lambda create-alias --function-name $functionName --function-version 1 --name staging --profile $awsProfile --region $awsRegion
+  if ($null -ne $EnvironmentVariables) {
+    if (Test-Path -Path $EnvironmentVariables) {
+      # Read environment variables from file
+      $json = Get-Content -Path $EnvironmentVariables
+      aws lambda update-function-configuration --function-name $functionName --environment Variables=$json --profile $awsProfile --region $awsRegion --output table
+    }
+    else {
+      Write-Output "$EnvironmentVariables doesn't exist, skipping"
+    }
+  }
+
+  # Publish a new version of the function and create aliases
+  aws lambda publish-version --function-name $functionName --profile $awsProfile --region $awsRegion --output table
+  aws lambda create-alias --function-name $functionName --function-version '$LATEST' --name production --profile $awsProfile --region $awsRegion --output table
+  aws lambda create-alias --function-name $functionName --function-version '$LATEST' --name staging --profile $awsProfile --region $awsRegion --output table
 }
