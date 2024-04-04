b7f02653f37e1375748afdcfc7c155b23eb11a11
ariffammobox <ariff.a@ammoboxstudios.com>
2024-04-04 12:19:33 +0800

    feat: added part to use `aws publish-version`

--- aws-cli/lambda/Set-LambdaEnv.ps1
+++ aws-cli/lambda/Set-LambdaEnv.ps1
@@ -28,14 +28,31 @@ foreach ($item in $items) {
 
     # Write-Output "update $($row.FunctionName) env with `n$envString"
 
-    $command = "aws lambda update-function-configuration --function-name $($row.FunctionName) --environment `"Variables={$envString}`" --profile $awsProfile --region $awsRegion | Out-Null"
+    $updateCommand = "aws lambda update-function-configuration --function-name $($row.FunctionName) --environment `"Variables={$envString}`" --profile $awsProfile --region $awsRegion | Out-Null"
 
-    Invoke-Expression $command 2>&1
+    Invoke-Expression $updateCommand 2>&1
 
     if ($LASTEXITCODE -ne 0) {
       Write-Output "Error updating function $($row.FunctionName)"
     }
 
+    do {
+      $functionState = aws lambda get-function --function-name $row.FunctionName --profile $awsProfile --region $awsRegion --output json | ConvertFrom-Json | % Configuration | % State
+      Start-Sleep -Seconds 1
+
+      if ($LASTEXITCODE -ne 0) {
+        Write-Output "Error getting function state $($row.FunctionName)"
+        break
+      }
+    } until ($functionState -eq "Active")
+
+    $publishCommand = "aws lambda publish-version --function-name $($row.FunctionName) --profile $awsProfile --region $awsRegion | Out-Null"
+
+    Invoke-Expression $publishCommand 2>&1
+
+    if ($LASTEXITCODE -ne 0) {
+      Write-Output "Error publishing new function version $($row.FunctionName)"
+    }
   }
 }
 
